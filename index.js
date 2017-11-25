e12d52b7e4fe0e077de7287357f4641100f1ae43
eviltik <eviltik@gmail.com>
2017-11-25 03:01:29 +0100

    default null for ip/ua

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Database = require('better-sqlite3');
 class UdgerParser {
     constructor(file) {
         this.db = new Database(file, {readonly:true, fileMustExist:true});
+        this.ip = null;
+        this.ua = null;
     }
 
     setUA(ua) {
